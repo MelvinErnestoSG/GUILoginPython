c367e21506173ad4212d0ce238c6c552c821e4a2
Melvin Ernesto Santana Garcia <melvinernestosantanagarcia15@gmail.com>
2023-03-01 16:35:42 -0500

    Update App.py

--- 01 Login Tkinter/App.py
+++ 01 Login Tkinter/App.py
@@ -1,55 +1,52 @@
-from tkinter import *
+from tkinter import*
 from tkinter import messagebox
 
-# create Tk window.
+# Create a new tkinter window for the login screen.
 root = Tk()
-root.title('Login')
+root.title('Window Login')
 root.eval('tk::PlaceWindow . center')
-root.geometry("200x280+800+300")
+root.geometry("250x107+640+370")
 root.resizable(False, False)
 
+# Add labels for username to the window.
+label_user = Label(root, text='Username')
+label_user.pack()
 
-# create Tk window 2.
+# Add username entry boxes to the window.
+entry_username = Entry(root)
+entry_username.pack()
 
+# Add labels for password to the window.
+label_pass = Label(root, text='Password')
+label_pass.pack()
 
-def access():
-    messagebox.showinfo("Access", "Welcome")
-    button = Button(root, text='Close', cursor='hand2', font='20', command=root.destroy)
-    button.pack(pady=10)
-
-
-def error():
-    messagebox.showerror("Error", "Username and password are incorrect.")
-    button = Button(root, text='Close', cursor='hand2', font='20', command=root.destroy)
-    button.pack(pady=10)
-
-
-user = StringVar()
-password = StringVar()
+# Add password entry boxes to the window.
+entry_password = Entry(root, show='*')
+entry_password.pack()
 
 
 def login():
-    user_name = user.get()
-    user_password = password.get()
+    # Function to verify login credentials.
+    user_name = entry_username.get()
+    user_password = entry_password.get()
+
+    # Clear the entry fields after login verification.
+    entry_username.delete(0, END)
+    entry_password.delete(0, END)
+
+    # Check if the entered username and password match a valid pair.
     if user_name == 'admin' and user_password == '1234':
-        access()
+        # If the login is successful, display a message and close the login window.
+        messagebox.showinfo('Login successful', 'Your username and password are correct.')
+        root.destroy()
     else:
-        error()
+        # If the login fails, display an error message
+        messagebox.showerror('Login failed', 'Incorrect username or password')
 
 
-labelLogin1 = Label(root, text='User Name', font='20')
-labelLogin1.pack(pady=10)
+# Add button to submit login information.
+button_login = Button(root, text='Enter', cursor='hand2', command=login)
+button_login.pack()
 
-entryLogin1 = Entry(root, textvariable=user,)
-entryLogin1.pack(pady=10)
-
-labelLogin2 = Label(root, text='Password', font='20')
-labelLogin2.pack(pady=10)
-
-entryLogin2 = Entry(root, textvariable=password, show='*')
-entryLogin2.pack(pady=10)
-
-buttonLogin = Button(root, text='Enter', cursor='hand2', font='20', command=login)
-buttonLogin.pack(pady=10)
-
+# Display the window.
 root.mainloop()
